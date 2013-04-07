f3f0caaa9eb181e8c4d4d8c72892ea39df1b7c3a
Tauseef <takhan@cs.brown.edu>
2013-04-07 16:43:02 -0400

    Beginnings of some data entry stuff

--- src/edu/brown/cs32/takhan/tag/User.java
+++ src/edu/brown/cs32/takhan/tag/User.java
@@ -1,5 +1,28 @@
 package edu.brown.cs32.takhan.tag;
 
+import com.google.common.collect.*;
+
 public class User {
+	
+	private String _userID;
+	private ListMultimap<String,String> _dataMap;
+	
+	
+	public User(){
+		_dataMap = ArrayListMultimap.create();
+		
+		
+	}
+	
+	
+	public String getID(){
+		return _userID;
+	}
+	
+	public void addData(String website, String tag){
+		if(!_dataMap.containsEntry(website, tag)){
+			_dataMap.put(website, tag);
+		}
+	}
 
 }
